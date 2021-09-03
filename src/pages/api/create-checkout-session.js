d03675e02c0b7af0aeec5c2f502ccf29b15bc821
Satya Ramesh Koppisetti <82876477+rameshkoppisetti@users.noreply.github.com>
2021-09-03 19:30:26 +0530

    Update create-checkout-session.js

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -7,7 +7,7 @@ export default async (req, res) => {
     quantity: 1,
     price_data: {
       currency: "inr",
-      unit_amount: item.price * 100 * 100,
+      unit_amount: item.price * 100,
       product_data: {
         name: item.title,
         images: [item.image],
