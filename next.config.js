055d85a0645eb424480bb3d8abf68f6ccdbfbca3
Satya Ramesh Koppisetti <82876477+rameshkoppisetti@users.noreply.github.com>
2021-09-05 13:56:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
         ],
     },
     env: {
-        stripe_public_key: STRIPE_PUBLIC_KEY,
+        stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
        
         
     },
