a08314aea4b09f1383979bd4cea8d95deeac77c4
Satya Ramesh Koppisetti <82876477+rameshkoppisetti@users.noreply.github.com>
2021-09-15 11:51:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC4e5DNG46bWf-imdvz7QM4Hzm_ezxS3so",
-    authDomain: "project-45888.firebaseapp.com",
-    projectId: "project-45888",
-    storageBucket: "project-45888.appspot.com",
-    messagingSenderId: "102055974494",
-    appId: "1:102055974494:web:b77ad6633825a86ec886cf",
-    measurementId: "G-4PVTE7DEKE",
+  apiKey: "AIzaSyBjNpj4j0brOF1SIW9kLo85_V8HMuRje5g",
+  authDomain: "fir-5ac3a.firebaseapp.com",
+  projectId: "fir-5ac3a",
+  storageBucket: "fir-5ac3a.appspot.com",
+  messagingSenderId: "95508128001",
+  appId: "1:95508128001:web:3b5d8d164e5634876f3b00",
+  measurementId: "G-46WFZYDHSF"
 };
-const app = !firebase.apps.length ?
-    firebase.initializeApp(firebaseConfig) :
-    firebase.app();
+
+const app = !firebase.apps.length
+    ? firebase.initializeApp(firebaseConfig)
+    : firebase.app();
 
 const db = app.firestore();
 
